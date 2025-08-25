e42b49eba136d9b6460ee8783eb59d7e5797637f
kazu <kazuhoh12@gmail.com>
2025-08-25 19:04:06 +0900

    todoのマイグレーション

---
+++ packages/db/prisma/migrations/20250825100217_add_todo/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Todo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `completed` BOOLEAN NOT NULL DEFAULT false,
+
+    INDEX `Todo_title_idx`(`title`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
