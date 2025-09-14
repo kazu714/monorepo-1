d763adc99729540a4148cc6faa64be65f130d710
kazu <kazuhoh12@gmail.com>
2025-09-14 11:27:20 +0900

    シンプルオブジェクトプラグインと、datetimeスカラ追加

--- apps/api/src/builder.ts
+++ apps/api/src/builder.ts
@@ -1,15 +1,20 @@
-import SchemaBuilder from '@pothos/core';
-import PrismaPlugin from '@pothos/plugin-prisma';
-import type PrismaTypes from '@pothos/plugin-prisma/generated';
-import type { Context } from './context';
-import { prisma } from './context'; 
+import SchemaBuilder from "@pothos/core";
+import PrismaPlugin from "@pothos/plugin-prisma";
+import SimpleObjectsPlugin from '@pothos/plugin-simple-objects';
+import type PrismaTypes from "@pothos/plugin-prisma/generated";
+import type { Context } from "./context";
+import { prisma } from "./context";
+import { DateTimeResolver } from "graphql-scalars";
 
 // Prismaプラグインを使用
 export const builder = new SchemaBuilder<{
   Context: Context;
   PrismaTypes: PrismaTypes;
+  Scalars: {
+    DateTime: { Input: Date; Output: Date };
+  };
 }>({
-  plugins: [PrismaPlugin],
+  plugins: [PrismaPlugin,SimpleObjectsPlugin],
   prisma: {
     client: prisma,
   },
@@ -18,3 +23,6 @@ export const builder = new SchemaBuilder<{
 // ルート型を作成（最低限必要）
 builder.queryType({});
 builder.mutationType({});
+
+// スカラーを追加
+builder.addScalarType("DateTime", DateTimeResolver, {});
